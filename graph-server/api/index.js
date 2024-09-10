a592c7abdf4ba2ec09066a98e920c9aef6e7c92a
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-09-09 22:42:51 -0300

    - oops my wrong

--- graph-server/api/index.js
+++ graph-server/api/index.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS.split(",");
 
 app.use(cors({
   origin: function (origin, callback) {
