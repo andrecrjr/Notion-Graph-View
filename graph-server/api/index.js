84926a190d038dfd6b751a16229a2d6507aa1937
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-09-09 23:00:49 -0300

    - trying to fix cors

--- graph-server/api/index.js
+++ graph-server/api/index.js
@@ -20,8 +20,7 @@ const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS.split(",");
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin) === -1) {
+    if (!allowedOrigins.includes(origin)) {
       const msg = 'A política de CORS não permite acesso deste domínio.';
       return callback(new Error(msg), false);
     }
